c65bbb6a3fcbadcb84434c43b672763c50bf996d
hsoo3844 <hsoo3844@naver.com>
2022-09-30 17:56:37 +0900

    add front-end

---
+++ ae-care/client/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Charts from './chart';
+
+function Main(props) {
+	return (
+		<>
+		<div className='mainMain'>
+			<div className='chartDiv'>
+				<Charts/>
+			</div>
+		</div>
+		</>
+	);
+};
+
+export default Main;
